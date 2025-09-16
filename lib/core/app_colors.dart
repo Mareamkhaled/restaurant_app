acc03b37eb48f803e877fb510d8165d8b4b7b00b
maream khaled ahmed <mareamkmousbah@gmail.com>
2025-09-16 15:27:07 +0300

    Add app_colors and app_images utility classes
    - Introduced AppColors class to centralize color definitions used throughout the app.
    - Added Assets class to manage image asset paths for better organization and maintainability.

---
+++ lib/core/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+abstract class AppColors {
+  static const Color orange = Color(0xffEC5500);
+  static const Color black = Color(0xff1A0900);
+  static const Color white = Color(0xffFFFCFA);
+  static const Color grey = Color(0xff4F4F4F);
+  static const Color green = Color(0xff00AB06);
+}
---
+++ lib/core/app_images.dart
@@ -0,0 +1,19 @@
+// ignore_for_file: prefer_single_quotes
+abstract class Assets {
+  Assets._();
+
+  static const String assetsImagesForgotpassword =
+      "assets/images/ForgotPassword.png";
+
+  static const String assetsImagesHunterFoodLogo =
+      "assets/images/HunterFoodLogo.png";
+
+  static const String assetsImagesLogo = "assets/images/logo.png";
+
+  static const String assetsImagesProfile = "assets/images/profile.png";
+
+  static const String assetsImagesSignup = "assets/images/signup.png";
+
+  static const String assetsImagesVertification =
+      "assets/images/vertification.png";
+}
